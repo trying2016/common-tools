6245a0ac3aab411f4645f7f074fccd3589611023
trying <wxytom@163.com>
2020-07-31 16:29:43 +0800

    fix count

--- utils/queue.go
+++ utils/queue.go
@@ -1,6 +1,9 @@
 package utils
 
-import "sync"
+import (
+	"sync"
+	"sync/atomic"
+)
 
 type QueueFun func(data interface{})
 
@@ -9,6 +12,7 @@ type Queue struct {
 	callBack   func(data interface{})
 	exitSignal chan struct{}
 	jobWaiter  *sync.WaitGroup
+	count      *int64
 }
 
 func NewQueue(size int, fun func(data interface{})) *Queue {
@@ -23,15 +27,18 @@ func (q *Queue) init(size int, fun func(data interface{})) {
 	q.queue = make(chan interface{}, size)
 	q.jobWaiter = &sync.WaitGroup{}
 	q.jobWaiter.Add(1)
+	var c int64
+	q.count = &c
 	go q.run()
 }
 
 func (q *Queue) Push(value interface{}) {
+	atomic.AddInt64(q.count, 1)
 	q.queue <- value
 }
 
-func (q *Queue) Count() int {
-	return len(q.queue)
+func (q *Queue) Count() int64 {
+	return *q.count
 }
 
 func (q *Queue) Destroy() {
@@ -43,6 +50,7 @@ func (q *Queue) run() {
 	for {
 		select {
 		case data := <-q.queue:
+			atomic.AddInt64(q.count, -1)
 			q.callBack(data)
 		case <-q.exitSignal:
 			q.jobWaiter.Done()
