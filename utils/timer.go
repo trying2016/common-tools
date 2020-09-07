a88803439d5649931cd29afcf124f17650f67b99
trying <wxytom@163.com>
2020-09-07 19:55:15 +0800

    commit code

--- utils/timer.go
+++ utils/timer.go
@@ -4,6 +4,19 @@ import (
 	"time"
 )
 
+func StartTimeWithChan(callback func(), interval int, exitSignal chan struct{}) {
+	go func() {
+		for {
+			select {
+			case <-time.After(time.Duration(interval) * time.Millisecond):
+				callback()
+			case <-exitSignal:
+				return
+			}
+		}
+	}()
+}
+
 func StartTime(callback func(), interval int) (exitSignal chan struct{}) {
 	exitSignal = make(chan struct{})
 	go func() {
