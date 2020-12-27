880706eeda7fe61efa0658d3b4fb7a397de7a00a
trying <wxytom@163.com>
2020-12-27 16:49:16 +0800

    handler 增加自定义数据

--- jsonRpc/rpcHandler.go
+++ jsonRpc/rpcHandler.go
@@ -100,7 +100,7 @@ func (handler *RpcHandler) Handle(ctx context.Context, conn *jsonrpc2.Conn, req 
 	if req.Method == "login" {
 		// 拦截登录
 		_, err := handler.methodHandle(req.Method, params)
-		if err != nil && err == ErrorNotHandle {
+		if err != nil && err != ErrorNotHandle {
 			if err := conn.Reply(ctx, req.ID, map[string]interface{}{
 				"status": "error",
 				"error":  err.Error(),
