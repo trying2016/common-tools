155a3955877e631ae3e773b5effe22dcfa82a4e6
trying <wxytom@163.com>
2021-01-11 18:05:06 +0800

    add get file list

--- utils/file.go
+++ utils/file.go
@@ -3,6 +3,8 @@ package utils
 import (
 	"io/ioutil"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 func DownloadFile(link, filePath string) (err error) {
@@ -56,4 +58,53 @@ func ReadAll(filePth string) ([]byte, error) {
 	}
 	defer f.Close()
 	return ioutil.ReadAll(f)
+}
+
+type FileList struct {
+	FilePath string
+	FileDir  string
+}
+
+func prepareDirs(dirs []string) ([]string, [][]os.FileInfo) {
+	resultDir := make([]string, 0)
+	resultDirFileInfo := make([][]os.FileInfo, 0)
+	for _, dir := range dirs {
+		if fi, err := os.Stat(dir); err != nil {
+			if !os.IsNotExist(err) {
+				continue
+			}
+			if err = os.MkdirAll(dir, 0700); err != nil {
+				continue
+			}
+		} else if !fi.IsDir() {
+			continue
+		}
+		if fis, err := ioutil.ReadDir(dir); err != nil {
+		} else {
+			resultDir = append(resultDir, dir)
+			resultDirFileInfo = append(resultDirFileInfo, fis)
+		}
+	}
+	return resultDir, resultDirFileInfo
+}
+
+func GetFileList(fileDir, suffix string) []*FileList {
+	arrFileLists := make([]*FileList, 0)
+	suffixUp := strings.ToUpper(suffix)
+	arrDirs, arrInfos := prepareDirs([]string{fileDir})
+	for idx, dbDir := range arrDirs {
+		for _, fi := range arrInfos[idx] {
+			fileName := fi.Name()
+			// try match suffix and `ordinal_pubKey_bitLength.suffix`
+			if !strings.HasSuffix(strings.ToUpper(fileName), suffixUp) {
+				continue
+			}
+			filePath := filepath.Join(dbDir, fileName)
+			arrFileLists = append(arrFileLists, &FileList{
+				FilePath: filePath,
+				FileDir:  dbDir,
+			})
+		}
+	}
+	return arrFileLists
 }
