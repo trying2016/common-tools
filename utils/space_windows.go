5acb9e4470c80025e32fbc9c26218f79cd42fdae
trying <wxytom@163.com>
2021-01-12 16:48:02 +0800

    add space util

---
+++ utils/space_windows.go
@@ -0,0 +1,35 @@
+package server
+
+import "C"
+
+import (
+	"fmt"
+	"syscall"
+	"unsafe"
+)
+
+type DiskStatus struct {
+	All uint64 `json:"all"`
+
+	Used uint64 `json:"used"`
+	Free uint64 `json:"free"`
+}
+
+const (
+	B  = 1
+	KB = 1024 * B
+	MB = 1024 * KB
+	GB = 1024 * MB
+)
+
+// disk usage of path/disk
+func DiskUsage(path string) (disk DiskStatus) {
+	h := syscall.MustLoadDLL("kernel32.dll")
+	c := h.MustFindProc("GetDiskFreeSpaceExW")
+	_, _, err := c.Call(uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(path))),
+		uintptr(unsafe.Pointer(&disk.Free)), uintptr(unsafe.Pointer(&disk.All)), uintptr(unsafe.Pointer(&disk.Used)))
+	if err != nil {
+		fmt.Println(err)
+	}
+	return
+}
