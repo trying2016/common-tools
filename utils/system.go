21a089b620f9cc6e460ec11e6d6a9a888f0ea1b7
trying <wxytom@163.com>
2021-02-07 00:58:35 +0800

    remove system

--- utils/system.go
+++ utils/system.go
@@ -1,38 +1,9 @@
 package utils
 
 import (
-	"os"
 	"os/exec"
 	"runtime"
-
-	"github.com/shirou/gopsutil/process"
 )
-
-type SystemInfomation struct {
-	Mem uint64
-	CPU float64
-}
-
-func GetSystemInformation() *SystemInfomation {
-	// get memory
-	getMem := func(pro *process.Process) uint64 {
-		processMemory, err := pro.MemoryInfo()
-		if err != nil {
-			return 0
-		}
-		return processMemory.RSS
-	}
-	curProcess, err := process.NewProcess(int32(os.Getpid()))
-	if err != nil {
-		return nil
-	}
-	mem := getMem(curProcess)
-	cpu, err := curProcess.CPUPercent()
-	if err == nil {
-		return &SystemInfomation{Mem: mem, CPU: 0}
-	}
-	return &SystemInfomation{Mem: mem, CPU: cpu}
-}
 
 // 打开网址
 func OpenUrl(url string) error {
